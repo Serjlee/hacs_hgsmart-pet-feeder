c633fb5b35922f7dff1ee59702a9e44ad5c7b767
Luca Osti <luca@osti.dev>
2026-01-14 13:22:27 +0000

    Actually make reconfigure to work

--- custom_components/hgsmart/config_flow.py
+++ custom_components/hgsmart/config_flow.py
@@ -39,7 +39,7 @@ class HGSmartConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         config_entry: config_entries.ConfigEntry,
     ) -> config_entries.OptionsFlow:
         """Get the options flow for this handler."""
-        return HGSmartOptionsFlow(config_entry)
+        return HGSmartOptionsFlow()
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
@@ -130,10 +130,6 @@ class HGSmartConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
 class HGSmartOptionsFlow(config_entries.OptionsFlow):
     """Handle options flow for HGSmart Pet Feeder."""
-
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
